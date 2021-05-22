91177539e586341eff7601de4bcccefd3afc489d
harveyall <allharvey@byui.edu>
2021-05-22 13:21:45 -0600

    Adding Armando's hello message.

--- src/laredo/Main.java
+++ src/laredo/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello My Name Is Enrique");
 	System.out.println("Hello From My Team");
+    System.out.println("Hello, my name is Armando 05/22/2021");
     }
 }
