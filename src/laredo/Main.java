f41ff748f1cc4e277c72e7c0240baef95e62f554
E-Laredo <enriquelaredo@byui.edu>
2021-05-22 13:12:07 -0600

    This is the 04 Prove for CS246

---
+++ src/laredo/Main.java
@@ -0,0 +1,9 @@
+package laredo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello My Name Is Enrique");
+	System.out.println("Hello From My Team");
+    }
+}
